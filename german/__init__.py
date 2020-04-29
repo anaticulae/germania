e78f9eac244a5659c769be9c83fd8ec079c0daa3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-29 21:43:53 +0200

    test(words): move words split tests from `words` package

--- german/__init__.py
+++ german/__init__.py
@@ -13,6 +13,7 @@ The `german` package is a wrapper for external tooling which have some
 `magic` inside to determine the type of a word or split a text into
 sentences into words.
 """
+
 import os
 
 from german.language import LanguageResult
--- tests/test_words_split.py
+++ tests/test_words_split.py
@@ -56,3 +56,72 @@ def test_split():
 def test_words_fromstr():
     splitted = german.words_fromstr(EXAMPLE)
     assert len(splitted) == 177, 'algo changed'  # Hint: Value is not correct.
+
+
+MERGE_DIVISION = """\
+kollektive Handlungssysteme der gesellschaftlichen
+Interessenartikulation. […] Als „Heraus-
+forderer“ machen sie Anliegen geltend, die im Prozess der politischen
+Willensbildung systema-
+tisch ausgeblendet werden. Sie stehen daher in konflikthafter
+Interaktion mit etablierten Akteu-
+ren – Institutionen und Organisationen – aus dem
+politisch-administrativen System (Hervorhe-
+bung im Original).
+"""
+
+
+def test_sentence_merge_with_textbreak():
+    sentences = german.split_sentences(MERGE_DIVISION)
+    assert len(sentences) == 3
+    assert 'Herausforderer' in sentences[1]
+    assert 'systematisch' in sentences[1]
+    assert 'Hervorhebung' in sentences[2]
+
+
+LINE_ENDING = """\
+Das Web als Service-Plattform: Verschiedene Dienste bieten die
+Möglichkeit, Arbeit über das Web zu organisieren; sie übernehmen
+Aufgaben, die ehemals Desktopanwendungen vorbehalten waren (z.B.
+Terminplanung, Dokument- bzw. Datenverwaltung etc.). Kollektive
+Intelligenz: Die Nutzer beteiligen sich und generieren gemeinsam
+Inhalte. Als Paradebeispiel gilt die Plattform Wikipedia, deren Artikel
+von Nutzern selbst verfasst bzw. verändert werden.
+"""
+
+
+def test_sentence_split_abrreviation_and_bracket():
+    sentences = german.split_sentences(LINE_ENDING)
+    assert len(sentences) == 5
+
+
+SHORT_SENTENCE = """\
+Das Web 2.0 gilt heute als eine Plattform, die sich vor allem durch die
+direkte Beteiligung der Nutzer und daraus entstehende Netzwerkeffekte,
+wie z.B. das Nutzen kollektiven Wissens auszeichnet. Partizipation und
+Kooperation sind wichtige Charakteristika des Web 2.0 – je mehr Nutzer
+beteiligt sind, desto besser wird der Dienst. Und: Durch
+Kundenbeteiligung und computergesteuertes Datenmanagement können
+Nischenmärkte und unscheinbare Webangebote im Long Tail zu kollektiver
+Stärke heranwachsen.
+"""
+
+
+def test_sentence_split_short():
+    sentences = german.split_sentences(SHORT_SENTENCE)
+    assert len(sentences) == 4
+
+
+MULTIPLE_SENTENCE_IN_QUOTATION = """\
+Auch der SPIEGEL beschreibt eine ähnliche Situation: Die Nutzer würden
+sich selbst entblättern, so ist in einem Leitartikel aus dem Jahr 2006
+zu lesen. Auf der Frontseite des Heftes titelt das Blatt entsprechend:
+„Ich im Internet. Wie sich die Menschheit online entblößt.“ In dem
+Artikel heißt es:
+"""
+
+
+def test_sentence_split_multiple_quoted_sentence():
+    sentences = german.split_sentences(MULTIPLE_SENTENCE_IN_QUOTATION)
+    assert len(sentences) == 5
+    assert sentences[-1] == 'In dem Artikel heißt es:', sentences[-1]
