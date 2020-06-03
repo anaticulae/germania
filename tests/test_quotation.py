e5456a53554a3c2f39829c2693f1a1ec0ed035f1
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-03 16:59:29 +0200

    test(quotation): add long quotation test

--- tests/test_quotation.py
+++ tests/test_quotation.py
@@ -43,3 +43,30 @@ def test_quotation_raw():
     splitted = german.split_words(second)
     raw = german.raw_quotation(splitted, expected)
     assert len(raw) == 1
+
+
+QUOTE_IN_TEXT = """\
+Auf Basis der beschriebenen Treiber für die Digitalisierung und im
+Hinblick auf die weiterführenden Kapitel erscheint jedoch folgende
+Definition nach BECKER ET Al. als zielführend für diese Bachelorarbeit:
+„Unter dem Begriff Digitalisierung verstehen wir die Transformation von
+Geschäftsmodellen mit Hilfe von Informations- und
+Kommunikationstechnologien zur Reduktion von Schnittstellen, zur
+funktionsübergreifenden Vernetzung und zur Erhöhung der Effektivität und
+Effizienz.“16 Die dargestellte Definition verdeutlicht, dass sich im
+Zuge der Digitalisierung die Geschäftsmodelle der mittelständischen
+Unternehmen verändern werden. Durch die funktionsübergreifende
+Vernetzung im Sinne von Industrie 4.0 sind alle Unternehmensbereiche
+davon betroffen.
+"""
+
+
+def test_parse_long_quote():
+    extracted = german.extract_quotes(QUOTE_IN_TEXT)
+    expected = [(29, 63)]
+    assert extracted == expected
+
+    splitted = german.split_words(QUOTE_IN_TEXT, validate_sentences=False)
+
+    raw = german.raw_quotation(splitted, extracted)
+    assert len(raw) == 1
