6c39f02c0e7c7e0bfcd0786799ca55fde5f874c3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-06 23:32:42 +0200

    0.5.2 automatically generated release

--- german/__init__.py
+++ german/__init__.py
@@ -35,6 +35,6 @@ from german.word import Words
 from german.word import contain_quotation_marks
 from german.word import split_words
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
