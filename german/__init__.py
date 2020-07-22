69cdd1223aa3a001c5ba42963300697449e6d70a
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-22 20:31:45 +0200

    feat(mail): add method mails to parse list of emails

--- german/__init__.py
+++ german/__init__.py
@@ -26,6 +26,7 @@ from german.pattern import matched
 from german.pattern.date import dates
 from german.pattern.date import years
 from german.pattern.href import hyperlink
+from german.pattern.mail import mails
 from german.pattern.pagination import pagenumbers
 from german.quotation import extract_quotes
 from german.quotation import raw_quotation
---
+++ german/pattern/mail.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import re
+
+import utila
+
+PATTERN = r"""
+[\.\w\-\_]+@[\w\.\-\_]+
+"""
+
+
+def mails(raw: str) -> list:
+    """\
+    >>> mails('This is email:1helmut.k.fahrendholz@mailbox.tu-berlin.de end.')
+    ['1helmut.k.fahrendholz@mailbox.tu-berlin.de']
+    """
+    result = []
+    for item in re.finditer(PATTERN, raw, flags=re.VERBOSE):
+        matched = utila.extract_match(item)
+        result.append(matched)
+    return result
