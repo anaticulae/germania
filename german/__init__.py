8bcf42fd35328ecd933d79c670ea674fea4027a8
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-14 13:13:51 +0200

    0.8.5 automatically generated release

--- german/__init__.py
+++ german/__init__.py
@@ -40,6 +40,6 @@ from german.word import Words
 from german.word import contain_quotation_marks
 from german.word import split_words
 
-__version__ = '0.8.4'
+__version__ = '0.8.5'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
