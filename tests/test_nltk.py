e7d97b541726c8ae7feaa26a31e189cbcf0edd0f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-22 13:09:56 +0100

    test(nltk): add nltk playground

---
+++ tests/test_nltk.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import nltk
+
+from tests.test_language import MIXED
+
+
+def test_tokenize_words():
+    tokens = list(nltk.word_tokenize(MIXED, language='german'))
+    assert len(tokens) == 105
+
+
+def test_tokenize_sentence():
+    sentences = list(nltk.sent_tokenize(MIXED, language='german'))
+    assert len(sentences) == 4
