5b5f3ecab8c2f6612572980cf0b883248fc7ec95
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-04-26 15:57:16 +0200

    test(pattern): refactor hyperlink unit test

--- german/pattern/href.py
+++ german/pattern/href.py
@@ -17,23 +17,15 @@ TABLE = str.maketrans({'∼': '~'})
 
 def hyperlink(raw: str, position: bool = False):
     r"""\
-    >>> hyperlink('Before: http://student.unifr.ch/\nReferenzrahmen2001.pdf after.')[0]
-    'http://student.unifr.ch/Referenzrahmen2001.pdf'
-    >>> hyperlink('This is a link:https://www.youtube.com/watch?v=RXbcAYxuZxw', True)[0]
-    ('https://www.youtube.com/watch?v=RXbcAYxuZxw', 15)
-    >>> hyperlink('Text.http://google.de')[0]
-    'http://google.de'
-    >>> hyperlink('Gemeinde Neunkirchen 31818\nwww.statistik.at/blickgem/fa1/g31818.pdf (03.12.2017')[0]
-    'www.statistik.at/blickgem/fa1/g31818.pdf'
-    >>> hyperlink('http://www.statistik.at/index.html?includePage=detailedView%C2%A7ionName=Bildung%2C+Kultur&pubId=461')
-    ['http://www.statistik.at/index.html?includePage=detailedView%C2%A7ionName=Bildung%2C+Kultur&pubId=461']
-    >>> hyperlink('10 Silla and Kaestner from http://www.ppgia.pucpr.br/∼silla/softwares/yasd.zip.' )
-    ['http://www.ppgia.pucpr.br/~silla/softwares/yasd.zip']
+    >>> hyperlink('Before: http://student.unifr.ch/\nReferenzrahmen2001.pdf after.', position=True)
+    [('http://student.unifr.ch/Referenzrahmen2001.pdf', 8)]
     """
     raw = raw.replace('\n', '')
     raw = raw.translate(TABLE)
+    # TODO: REPLACE THIS PATTERN
     pattern = r"""
-    (http://|https://|www)[\w\d\./\-\?\=\&\%\+\~]+[\w\d/\?\=\&\%]\b  # no dot at the end
+    (https://|http://|www\.)
+    [\w\d\./\-\?\=\&\%\+\~]+[\w\d/\?\=\&\%]\b  # no dot at the end
     """
     result = []
     for item in re.finditer(pattern, raw, flags=re.VERBOSE):
---
+++ tests/pattern/test_hyperlink.py
@@ -0,0 +1,53 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import pytest
+import utila
+
+import german
+
+HYPERLINKS = """\
+Before: http://student.unifr.ch/
+Referenzrahmen2001.pdf after.
+http://student.unifr.ch/Referenzrahmen2001.pdf
+
+This is a link:https://www.youtube.com/watch?v=RXbcAYxuZxw
+https://www.youtube.com/watch?v=RXbcAYxuZxw
+
+Text.http://google.de
+http://google.de
+
+Gemeinde Neunkirchen 31818\nwww.statistik.at/blickgem/fa1/g31818.pdf (03.12.2017)
+www.statistik.at/blickgem/fa1/g31818.pdf
+
+http://www.statistik.at/index.html?includePage=detailedView%C2%A7ionName=Bildung%2C+Kultur&pubId=461
+http://www.statistik.at/index.html?includePage=detailedView%C2%A7ionName=Bildung%2C+Kultur&pubId=461
+
+10 Silla and Kaestner from http://www.ppgia.pucpr.br/∼silla/softwares/yasd.zip.
+http://www.ppgia.pucpr.br/~silla/softwares/yasd.zip
+
+"""
+
+
+def prepared():
+    result = []
+    for index, doubleline in enumerate(HYPERLINKS.split('\n\n')):
+        if not doubleline.strip():
+            break
+        raw, expected = doubleline.rsplit('\n', maxsplit=1)
+        raw, expected = raw.strip(), expected.strip()
+        expected = expected.split(' ')
+        result.append(pytest.param(raw, expected, id=str(index)))
+    return result
+
+
+@pytest.mark.parametrize('raw, expected', prepared())
+def test_hyperlinks(raw, expected):
+    parsed = german.hyperlink(raw)
+    assert parsed == expected
