684632d152e8ad9bdc3b1f2230bda63bc566e733
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-04-27 15:29:29 +0200

    test(data): verify loading no person dict

---
+++ tests/test_data.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import german_data
+
+
+def test_noperson():
+    assert german_data.NOPERSON
+    assert 'din' in german_data.NOPERSON
