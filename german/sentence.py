b6c37bb813c3383f898bf20816d4d6b158f01030
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-12 11:03:08 +0200

    remove(unused): remove unused import

--- german/sentence.py
+++ german/sentence.py
@@ -7,8 +7,6 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import re
-
 import knlp
 import konrad
 import utila
